c9ad9e62443b67148b73668d703498c0b16c569f
Zhdanov <dedkot01@gmail.com>
2021-05-06 12:01:54 +0300

    added JsonFormats for LocalDate

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
-import sbt._
 import sbt.Keys._
+import sbt._
 
 lazy val root =
   Project(id = "root", base = file("."))
--- data-ingress/src/main/scala/org/dedkot/DataIngress.scala
+++ data-ingress/src/main/scala/org/dedkot/DataIngress.scala
@@ -3,9 +3,9 @@ package org.dedkot
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import cloudflow.akkastream._
 import cloudflow.akkastream.util.scaladsl._
-
 import cloudflow.streamlets._
 import cloudflow.streamlets.avro._
+import org.dedkot.SubscriptionDataJsonSupport._
 
 class DataIngress extends AkkaServerStreamlet {
   val out   = AvroOutlet[SubscriptionData]("out").withPartitioner(RoundRobinPartitioner)
