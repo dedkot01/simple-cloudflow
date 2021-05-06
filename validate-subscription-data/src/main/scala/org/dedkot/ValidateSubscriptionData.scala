96b1a441ec4de3067a3708bf62b0c0b2f22b9670
Zhdanov <dedkot01@gmail.com>
2021-05-06 12:37:11 +0300

    wow, it's working!

--- validate-subscription-data/src/main/scala/org/dedkot/ValidateSubscriptionData.scala
+++ validate-subscription-data/src/main/scala/org/dedkot/ValidateSubscriptionData.scala
@@ -2,22 +2,20 @@ package org.dedkot
 
 import cloudflow.akkastream._
 import cloudflow.akkastream.scaladsl._
-import cloudflow.akkastream.util.scaladsl._
 import cloudflow.streamlets._
 import cloudflow.streamlets.avro._
 
 class ValidateSubscriptionData extends AkkaStreamlet {
-  val in      = AvroInlet[SubscriptionData]("in")
-  val valid   = AvroOutlet[SubscriptionData]("valid").withPartitioner(RoundRobinPartitioner)
-  val invalid = AvroOutlet[InvalidSubscriptionData]("invalid").withPartitioner(RoundRobinPartitioner)
-  val shape   = StreamletShape(in).withOutlets(invalid, valid)
+  val in    = AvroInlet[SubscriptionData]("in")
+  val shape = StreamletShape.withInlets(in)
 
   override def createLogic = new RunnableGraphStreamletLogic() {
-    def runnableGraph = sourceWithCommittableContext(in).to(Splitter.sink(flow, invalid, valid))
+    def runnableGraph = sourceWithCommittableContext(in).via(flow).to(committableSink)
     def flow =
       FlowWithCommittableContext[SubscriptionData].map { subData =>
-        if (subData.duration < 0) Left(InvalidSubscriptionData(subData, "duration must be positive"))
-        else Right(subData)
+        if (subData.duration < 0) system.log.warning(s"Bad! $subData")
+        else system.log.info(s"Nice! $subData")
+        subData
       }
   }
 }
